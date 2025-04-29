a32958f5f1c418b7b060d0db1792c7c30c424e92
nnra6864 <enenragd@gmail.com>
2025-04-29 19:37:48 +0200

    Added support for string literals(true, false), properties and fields(only static working rn)

--- Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_Validators/Artifice_CustomAttributeDrawer_ValidateInputAttribute.cs
+++ Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_Validators/Artifice_CustomAttributeDrawer_ValidateInputAttribute.cs
@@ -36,27 +36,87 @@ namespace ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttribute
             var condition = validateAttribute.Condition;
             _logMessage = validateAttribute.Message;
 
+            // Check for literal strings
             switch (condition.ToLower())
             {
                 case "true": return true;
                 case "false": return false;
             }
-
+            
+            // Check for validation property
+            var validationMembers = targetType.GetMember(condition,
+                BindingFlags.Instance | BindingFlags.Static |
+                BindingFlags.Public | BindingFlags.NonPublic);
+            if (validationMembers is { Length: > 0 })
+                return ExecuteValidationMember(validationMembers, targetObject);
+            
+            // Check for validation method
             var validationMethod = targetType.GetMethod(condition,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
+                BindingFlags.Instance | BindingFlags.Static |
+                BindingFlags.Public | BindingFlags.NonPublic);
             if (validationMethod != null)
                 return ExecuteValidationMethod(
-                    validationMethod, targetObject, targetType, fieldName);
-            else
+                    validationMethod, targetObject, fieldName);
+            
+            _logMessage = $"ValidateInput: Invalid validation condition: '{condition}'";
+            return false;
+        }
+
+        private bool ExecuteValidationMember(MemberInfo[] validationMembers, object targetObject)
+        {
+            var member = validationMembers[0];
+            var memberName = member.Name;
+
+            try
             {
-                _logMessage = $"ValidateInput: Invalid validation condition: '{condition}'";
+                if (member is FieldInfo fieldInfo)
+                {
+                    if (fieldInfo.FieldType != typeof(bool))
+                    {
+                        _logMessage =
+                            $"ValidateInput: Validation field must be a bool: '{memberName}'";
+                        return false;
+                    }
+
+                    var value = fieldInfo.GetValue(fieldInfo.IsStatic ? null : targetObject);
+                    return value != null && (bool)value;
+                }
+
+                if (member is PropertyInfo propertyInfo)
+                {
+                    if (propertyInfo.PropertyType != typeof(bool))
+                    {
+                        _logMessage =
+                            $"ValidateInput: Validation property must be a bool: '{memberName}'";
+                        return false;
+                    }
+
+                    if (!propertyInfo.CanRead)
+                    {
+                        _logMessage =
+                            $"ValidateInput: Validation property must be readable: '{memberName}'";
+                        return false;
+                    }
+
+                    var value = propertyInfo.GetValue(propertyInfo.GetMethod.IsStatic
+                        ? null : targetObject);
+                    return value != null && (bool)value;
+                }
+
+                _logMessage =
+                    $"ValidateInput: Validation member must be a field or property: '{memberName}'";
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _logMessage =
+                    $"ValidateInput: Exception occurred while accessing validation member" +
+                    $" '{memberName}'.\nException: {ex.Message}";
                 return false;
             }
         }
-
-        private bool ExecuteValidationMethod(MethodInfo validationMethod, object targetObject,
-                                             Type targetType, string fieldName)
+        
+        private bool ExecuteValidationMethod(MethodInfo validationMethod, object targetObject, string fieldName)
         {
             var methodName = validationMethod.Name;
 
@@ -76,7 +136,7 @@ namespace ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttribute
             {
                 _logMessage =
                     $"ValidateInput: Exception occurred while invoking validation method" +
-                    $"'{methodName}' for field '{fieldName}'." +
+                    $" '{methodName}' for field '{fieldName}'." +
                     $"\nException: {ex.InnerException?.Message ?? ex.Message}";
                 return false;
             }
@@ -84,7 +144,7 @@ namespace ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttribute
             {
                 _logMessage =
                     $"ValidateInput: Exception occured while executing validation method" +
-                    $"'{methodName}' for field '{fieldName}'.\nException: {ex.Message}";
+                    $" '{methodName}' for field '{fieldName}'.\nException: {ex.Message}";
                 return false;
             }
 
