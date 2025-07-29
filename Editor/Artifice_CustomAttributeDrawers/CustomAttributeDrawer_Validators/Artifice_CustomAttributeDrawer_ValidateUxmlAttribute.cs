6a20f6fb3efef62587b23eca079dcbf0c33f1802
Zacharias Pervolarakis <39521184+ZackPer@users.noreply.github.com>
2025-07-29 12:39:56 +0300

    created ValidateUxml and ValidateJson

---
+++ Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_Validators/Artifice_CustomAttributeDrawer_ValidateUxmlAttribute.cs
@@ -0,0 +1,40 @@
+using System.Xml;
+using ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers;
+using ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttributeDrawer_Validators;
+using ArtificeToolkit.Editor.Resources;
+using CustomAttributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Artifice_CustomAttributeDrawers.CustomAttributeDrawer_Validators
+{
+    [Artifice_CustomAttributeDrawer(typeof(ValidateUxmlAttribute))]
+    public class Artifice_CustomAttributeDrawer_ValidateUxmlAttribute : Artifice_CustomAttributeDrawer_Validator_BASE
+    {
+        public override string LogMessage { get; } = "Value is not in UXML format.";
+        public override Sprite LogSprite { get; } = Artifice_SCR_CommonResourcesHolder.instance.ErrorIcon;
+        public override LogType LogType { get; } = LogType.Error;
+        
+        protected override bool IsApplicableToProperty(SerializedProperty property)
+        {
+            return property.propertyType == SerializedPropertyType.String;
+        }
+
+        public override bool IsValid(SerializedProperty property)
+        {
+            if (string.IsNullOrWhiteSpace(property.stringValue))
+                return false;
+
+            try
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(property.stringValue);
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Runtime/CustomAttributes/ValidateJsonAttribute.cs
@@ -0,0 +1,8 @@
+using ArtificeToolkit.Attributes;
+
+namespace CustomAttributes
+{
+    public class ValidateJsonAttribute : ValidatorAttribute
+    {
+    }
+}
