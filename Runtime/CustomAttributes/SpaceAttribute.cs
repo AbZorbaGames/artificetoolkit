28fd3400a893f1de7fb3c9048e5d1962db9b3c41
Zacharias Pervolarakis <39521184+ZackPer@users.noreply.github.com>
2025-09-09 14:31:18 +0300

    Now, when custom attributes are fetched, attributes from the class definition or interface definion are also counted.

--- Runtime/CustomAttributes/SpaceAttribute.cs
+++ Runtime/CustomAttributes/SpaceAttribute.cs
@@ -1,6 +1,9 @@
+using System;
+
 namespace ArtificeToolkit.Attributes
 {
     /// <summary> Adds space in the given direction. </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Method, AllowMultiple = true)]
     public class SpaceAttribute : CustomAttribute, IArtifice_ArrayAppliedAttribute
     {
         public readonly int ValueTop;
