69c1a95f4df75070f3bd626395c0e40406a7c6c5
Zacharias Pervolarakis <39521184+ZackPer@users.noreply.github.com>
2025-12-15 11:02:22 +0200

    fixed reaonly attribute to be up to date

--- Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_ReadOnlyAttribute/Artifice_CustomAttributeDrawer_ReadOnlyAttribute.cs
+++ Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_ReadOnlyAttribute/Artifice_CustomAttributeDrawer_ReadOnlyAttribute.cs
@@ -7,36 +7,10 @@ namespace ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttribute
     [Artifice_CustomAttributeDrawer(typeof(ReadOnlyAttribute))]
     public class Artifice_CustomAttributeDrawer_ReadOnlyAttribute : Artifice_CustomAttributeDrawer
     {
-        // Add a semi transparent blocker and disable interactability.
+        // Set the property field enabled to false.
         public override void OnPropertyBoundGUI(SerializedProperty property, VisualElement propertyField)
         {
-            // Get style
-            propertyField.styleSheets.Add(Artifice_Utilities.GetStyle(GetType()));
-            
-            // Field generic base input field and draw a semi transparent rect above it.
-            var inputFields = propertyField.Query(className: "unity-property-field__input").ToList();
-            foreach(var inputField in inputFields)
-            {
-                var transparentBlocker = new VisualElement();
-                transparentBlocker.AddToClassList("blocker");
-                inputField.Add(transparentBlocker);
-                
-                transparentBlocker.tooltip = "This property is marked as Read-Only.\n";
-            }
-
-            // Make all input field non interactable.
-            DisableInteractivity(propertyField);
-        }
-        
-        // Recursively disable interactivity for all elements
-        private void DisableInteractivity(VisualElement element)
-        {
-            // Set the picking mode to Ignore to make elements non-interactable
-            element.pickingMode = PickingMode.Ignore;
-
-            // If there are child elements, recursively disable them as well
-            foreach (var child in element.Children())
-                DisableInteractivity(child);
+            propertyField.SetEnabled(false);
         }
     }
 }
