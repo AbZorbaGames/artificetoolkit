a2241816939fc98b4aef620c799561e87c92d37e
Zacharias Pervolarakis <39521184+ZackPer@users.noreply.github.com>
2026-03-05 16:21:16 +0200

    Updated so that it is supported for unity 2022+

--- Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_LayoutPixelsAttribute/Artifice_CustomAttributeDrawer_LayoutPixelsAttribute.cs
+++ Editor/Artifice_CustomAttributeDrawers/CustomAttributeDrawer_LayoutPixelsAttribute/Artifice_CustomAttributeDrawer_LayoutPixelsAttribute.cs
@@ -10,21 +10,21 @@ namespace ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttribute
         public override VisualElement OnWrapGUI(SerializedProperty property, VisualElement root)
         {
             var attribute = (LayoutPixelsAttribute)Attribute;
-            
+
             var container = new VisualElement();
             container.name = $"Layout Percent ({property.displayName})";
-            
+
             container.style.flexGrow = 0;
             container.style.flexShrink = 0;
 
             // Apply style
-            if(attribute.WidthPixels.HasValue)
-                container.style.width = new StyleLength(Length.Pixels(attribute.WidthPixels.Value));
+            if (attribute.WidthPixels.HasValue)
+                container.style.width = new StyleLength(new Length(attribute.WidthPixels.Value, LengthUnit.Pixel));
             if(attribute.HeightPixels.HasValue)
-                container.style.height = new StyleLength(Length.Pixels(attribute.HeightPixels.Value));
-            
+                container.style.height = new StyleLength(new Length(attribute.HeightPixels.Value, LengthUnit.Pixel));
+
             container.Add(root);
-            
+
             return container;
         }
     }
